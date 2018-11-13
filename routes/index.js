a2825b031322f4b3d18b5a49315c6e8bd978a197
Yosapat Thamarongrak <plumeria.in.sky@gmail.com>
2018-11-14 01:12:10 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var reply;
+
+router.post('/device', function(req, res){
+    reply = req.body;
+    res.send(reply);
+});
+
+router.get('/get-device', function(req, res) {
+  res.send(reply);
+});
+
+router.get('/show-bay-face', function(req, res){
+    res.render('index');
+});
+module.exports = router;
